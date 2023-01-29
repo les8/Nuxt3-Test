9dcd3b2b782b6a4923cd22d6a0fb650bd9effdeb
Leonid Skurikhin <leonid.skurihin@yandex.ru>
2023-01-29 15:32:06 +0300

    Динамический путь

---
+++ pages/products/[id].vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h3>Продукт {{ id }}</h3>
+  </div>
+</template>
+
+<script setup lang="ts">
+//на данный момент можно вибить любой урл по такой системе и он отработает, нужно понять как блокировать несуществующие пути
+ const { id } = useRoute().params;
+ console.log(useRoute());
+ 
+</script>
+
+<style scoped>
+
+</style>
