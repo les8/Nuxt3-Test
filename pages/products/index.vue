2af8455acf5780392b388fd6a83677d843f07ce7
Leonid Skurikhin <leonid.skurihin@yandex.ru>
2023-01-31 21:38:46 +0300

    ДОбавил продукты

--- pages/products/index.vue
+++ pages/products/index.vue
@@ -1,13 +1,21 @@
 <template>
   <div>
     <h1>Products</h1>
+
+    <ul>
+      <li v-for="p in products">
+        <NuxtLink :to="`products/${p.id}`">{{ p.title }}</NuxtLink>
+      </li>
+    </ul>
   </div>
 </template>
 
-<script lang="ts" setup>
+<script setup>
   definePageMeta({
     layout: 'products',
   });
+
+  const {data: products} = await useFetch('https://fakestoreapi.com/products')
 </script>
 
 <style lang="scss" scoped>
