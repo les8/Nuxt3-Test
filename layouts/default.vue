8d55ccc6dce37d02518bb5e200f5a9a148743bc6
Leonid Skurikhin <leonid.skurihin@yandex.ru>
2023-01-31 20:34:32 +0300

    default layout

---
+++ layouts/default.vue
@@ -0,0 +1,16 @@
+<template>
+  <header>
+    <nav>
+      <ul>
+        <li>
+          <NuxtLink to="/about">About</NuxtLink>
+        </li>
+        <li>
+          <NuxtLink to="/products">Products</NuxtLink>
+        </li>
+      </ul>
+    </nav>
+  </header>
+
+  <slot />
+</template>
