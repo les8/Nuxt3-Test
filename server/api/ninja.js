149b8841cf592852e355f7ec7a84761ab8ad0d32
Leonid Skurikhin <leonid.skurihin@yandex.ru>
2023-02-05 15:14:56 +0300

    fix

--- server/api/ninja.js
+++ server/api/ninja.js
@@ -1,4 +1,4 @@
-export default defineEventHandler(async (event) => {
+export default defineEventHandler(async () => {
   //api call with private key
   const { data } = await $fetch('https://api.currencyapi.com/v3/latest?apikey=EO5k7xOcI8ct4qh1AnZbpVRgXJc6FaIObgjqJjr9&currencies=EUR%2CUSD%2CCAD')
 
