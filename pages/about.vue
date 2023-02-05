5aa26fa8cf8bf100a32e4d4daa74ab8ac74aa97f
Leonid Skurikhin <leonid.skurihin@yandex.ru>
2023-02-05 15:05:56 +0300

    server routes

--- pages/about.vue
+++ pages/about.vue
@@ -1,7 +1,13 @@
 <template>
   <h1>About page here</h1>
+  {{ data }}
 </template>
 
-<script lang="ts" setup>
-
+<script setup>
+const { data } = await useFetch('/api/ninja?name=mario', {
+  method: 'post',
+  body: {
+    age: 30,
+  },
+});
 </script>
